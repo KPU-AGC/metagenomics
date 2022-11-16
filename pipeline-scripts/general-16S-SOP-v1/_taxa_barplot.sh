4321f785006915c6e087eecfbd066c6b24d67089
Erick Samera <erick.samera@gmail.com>
2022-11-16 01:12:50 -0800

    fix: adjusted filtering

--- pipeline-scripts/general-16S-SOP-v1/_taxa_barplot.sh
+++ pipeline-scripts/general-16S-SOP-v1/_taxa_barplot.sh
@@ -6,7 +6,7 @@ OUTPUT_DIR=$2
 METADATA=$3
 
 qiime taxa barplot \
-    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered.qza \
     --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
     --m-metadata-file $METADATA \
     --o-visualization $OUTPUT_DIR/taxa/taxa_barplot.qzv
--- pipeline-scripts/general-16S-SOP-v1/_taxonomy_filtering.sh
+++ pipeline-scripts/general-16S-SOP-v1/_taxonomy_filtering.sh
@@ -4,24 +4,23 @@
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 
-time qiime feature-table filter-features \
-    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered.qza \
-    --p-filter-empty-samples \
-    --p-min-frequency 1 \
-    --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_filtered_again.qza \
-    --verbose
+# time qiime taxa filter-table \
+#     --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered_again.qza \
+#     --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
+    
+#     --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza
 
-time qiime taxa filter-table \
-    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered_again.qza \
-    --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
-    --p-include p__ \
-    --p-exclude mitochondria,chloroplast \
-    --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza
+# time qiime taxa filter-table \
+#     --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered_again.qza \
+#     --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
+#     --p-include p__ \
+#     --p-exclude mitochondria,chloroplast \
+#     --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza
 
 time qiime feature-table summarize \
-    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza \
-    --o-visualization $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qzv
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered.qza \
+    --o-visualization $OUTPUT_DIR/cluster-output/clustered_table_filt.qzv
 
 time qiime tools export \
-    --input-path $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza \
+    --input-path $OUTPUT_DIR/cluster-output/clustered_table_filtered.qza \
     --output-path $OUTPUT_DIR/exported-feature-table
