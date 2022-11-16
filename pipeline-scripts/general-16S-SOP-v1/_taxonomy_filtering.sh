9ba7178f35878ddfb439ea499e3d5d670264fb9d
Erick Samera <erick.samera@gmail.com>
2022-11-16 00:16:20 -0800

    feat: taxonomy filtering

--- pipeline-scripts/general-16S-SOP-v1/_taxonomy_filtering.sh
+++ pipeline-scripts/general-16S-SOP-v1/_taxonomy_filtering.sh
@@ -4,17 +4,23 @@
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 
-qiime taxa filter-table \
+time qiime feature-table filter-features \
     --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered.qza \
+    --p-filter-empty-samples \
+    --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_filtered_again.qza \
+    --verbose
+
+time qiime taxa filter-table \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table_filtered_again.qza \
     --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
     --p-include p__ \
     --p-exclude mitochondria,chloroplast \
     --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza
 
-qiime feature-table summarize \
+time qiime feature-table summarize \
     --i-table $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza \
     --o-visualization $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qzv
 
-qiime tools export \
+time qiime tools export \
     --input-path $OUTPUT_DIR/cluster-output/clustered_table_filt_decontam.qza \
     --output-path $OUTPUT_DIR/exported-feature-table
