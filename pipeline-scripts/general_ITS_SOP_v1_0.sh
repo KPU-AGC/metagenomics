7bc1e5059d13e0475bfbca3e3dbccceb92631cbf
Erick Samera <erick.samera@gmail.com>
2022-11-17 01:00:12 -0800

    created file

---
+++ pipeline-scripts/general_ITS_SOP_v1_0.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# general_ITS_SOP_v1_0.sh (qiime2-2022.8)
+
+# Program performs a general qiime2 (qiime2-2022.8) pipeline and eventually produces a taxa bar plot
+# and rooted phylogenetic tree.
+#
+# Make sure that the proper conda environment is active before running this script.
+
+#------------ I/O Variables -----------#
+INPUT_DIR="raw-data"
+OUTPUT_DIR="output"
+
+#-------------- Variables -------------#
+METADATA="metadata.tab"
+NCORES=8
+REFERENCE_SEQUENCES="../resources/unite-ver9/unite-ver9-seqs.qza"
+CLASSIFIER="../resources/classifiers/unite-ver9-99-classifier.qza"
+
+mkdir $OUTPUT_DIR
+
+qiime tools import \
+    --type SampleData[PairedEndSequencesWithQuality] \
+    --input-path $INPUT_DIR \
+    --output-path $OUTPUT_DIR/reads.qza \
+    --input-format CasavaOneEightSingleLanePerSampleDirFmt
+
+qiime cutadapt trim-paired \
+    --i-demultiplexed-sequences $OUTPUT_DIR/reads.qza \
+    --p-anywhere-f CTTGGTCATTTAGAGGAAGTAA \
+    --p-anywhere-r GCTGCGTTCTTCATCGATGC \
+    --p-discard-untrimmed \
+    --p-no-indels \
+    --p-cores $NCORES \
+    --o-trimmed-sequences $OUTPUT_DIR/reads_trimmed.qza
+
+qiime vsearch join-pairs \
+    --i-demultiplexed-seqs $OUTPUT_DIR/reads_trimmed.qza \
+    --o-joined-sequences $OUTPUT_DIR/reads_trimmed_joined.qza
+
+qiime quality-filter q-score \
+    --i-demux $OUTPUT_DIR/reads_trimmed_joined.qza \
+    --o-filter-stats $OUTPUT_DIR/filt_stats.qza \
+    --o-filtered-sequences $OUTPUT_DIR/reads_trimmed_joined_filt.qza
+
+qiime deblur denoise-other \
+    --i-demultiplexed-seqs $OUTPUT_DIR/reads_trimmed_joined_filt.qza \
+    --i-reference-seqs $REFERENCE_SEQUENCES \
+    --p-trim-length 270 \
+    --p-sample-stats \
+    --p-min-reads 1 \
+    --p-jobs-to-start $NCORES \
+    --output-dir $OUTPUT_DIR/deblur-output
+
+qiime vsearch cluster-features-open-reference \
+    --i-sequences $OUTPUT_DIR/deblur-output/representative_sequences.qza \
+    --i-table $OUTPUT_DIR/deblur-output/table.qza \
+    --i-reference-sequences $REFERENCE_SEQUENCES \
+    --p-perc-identity 0.97 \
+    --p-threads $NCORES \
+    --output-dir $OUTPUT_DIR/cluster-output
+
+qiime vsearch uchime-denovo \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table.qza \
+    --i-sequences $OUTPUT_DIR/cluster-output/clustered_sequences.qza \
+    --output-dir $OUTPUT_DIR/chimera-filter-output
+
+qiime feature-table filter-features \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table.qza \
+    --m-metadata-file $OUTPUT_DIR/chimera-filter-output/nonchimeras.qza \
+    --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_nonchimeric.qza
+qiime feature-table filter-seqs \
+    --i-data $OUTPUT_DIR/cluster-output/clustered_sequences.qza \
+    --m-metadata-file $OUTPUT_DIR/chimera-filter-output/nonchimeras.qza \
+    --o-filtered-data $OUTPUT_DIR/cluster-output/clustered_sequences_nonchimeric.qza
+
+qiime feature-classifier classify-sklearn \
+    --i-reads $OUTPUT_DIR/cluster-output/clustered_sequences_nonchimeric.qza \
+    --i-classifier $CLASSIFIER \
+    --p-n-jobs $NCORES \
+    --output-dir $OUTPUT_DIR/taxa
+
+qiime taxa filter-table \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table_nonchimeric.qza \
+    --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
+    --p-include p__ \
+    --p-exclude mitochondria,chloroplast \
+    --o-filtered-table $OUTPUT_DIR/cluster-output/clustered_table_nonchimeric_decontam.qza
+
+qiime feature-table summarize \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table_nonchimeric_decontam.qza \
+    --o-visualization $OUTPUT_DIR/cluster-output/clustered_table_nonchimeric_decontam.qzv
+
+qiime taxa barplot \
+    --i-table $OUTPUT_DIR/cluster-output/clustered_table_nonchimeric_decontam.qza \
+    --i-taxonomy $OUTPUT_DIR/taxa/classification.qza \
+    --m-metadata-file $METADATA \
+    --o-visualization $OUTPUT_DIR/taxa/taxa_barplot.qzv
+
+qiime phylogeny align-to-tree-mafft-fasttree \
+    --i-sequences $OUTPUT_DIR/cluster-output/clustered_sequences_nonchimeric.qza \
+    --p-n-threads $NCORES \
+    --output-dir $OUTPUT_DIR/tree
