66e32fc6953a3e60988bb0ee3f48b9965bd3dd38
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2024-04-16 21:20:17 -0700

    FIX: wrong variable

--- src/main.sh
+++ src/main.sh
@@ -6,7 +6,7 @@ FASTQ_DIR="/home/erick/projects/one-offs/20240416_edna/fastq"
 QIIME_OUT_DIR="/home/erick/projects/one-offs/20240416_edna/qiime_output"
 QIIME_DATA="/home/erick/data/qiime"
 
-./src/_generate_manifest.sh ${FASTQ_DIR}
+#./src/_generate_manifest.sh ${FASTQ_DIR}
 
 ./src/_import_data.sh \
   FASTQ_DIR=${FASTQ_DIR} \
@@ -14,9 +14,9 @@ QIIME_DATA="/home/erick/data/qiime"
   QIIME_DATA=${QIIME_DATA} \
   DENOISE="DADA2"
 
-read -p "Mean sample depth: " BLEEDTHROUGH_VALUE
+read -p "Mean sample depth: " SAMPLE_DEPTH
 ./src/_output_taxa_barplot.sh \
   FASTQ_DIR=${FASTQ_DIR} \
   QIIME_OUT_DIR=${QIIME_OUT_DIR} \
   QIIME_DATA=${QIIME_DATA} \
-  BLEEDTHROUGH_VALUE="${BLEEDTHROUGH_VALUE}"
+  SAMPLE_DEPTH="${SAMPLE_DEPTH}"
