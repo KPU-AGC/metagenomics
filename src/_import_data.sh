125c2eafcb1a5450e3ec745cfd9cfdf21c17513b
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2024-04-16 21:06:18 -0700

    FEAT: combined denoising scripts

--- src/_import_data.sh
+++ src/_import_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Perform a series of standardized QIIME2 commands for importing data.
+set -e
 
 for ARGUMENT in "$@"; do
    KEY=$(echo $ARGUMENT | cut -f1 -d=)
@@ -17,14 +18,39 @@ qiime tools import \
   --output-path ${QIIME_OUT_DIR}/seqs_paired_end.qza \
   --input-format PairedEndFastqManifestPhred33V2
 
-qiime dada2 denoise-paired \
-  --i-demultiplexed-seqs ${QIIME_OUT_DIR}/seqs_paired_end.qza \
-  --p-trunc-len-f 250 \
-  --p-trunc-len-r 210 \
-  --p-max-ee-f 2 \
-  --p-max-ee-r 3 \
-  --p-n-threads 16 \
-  --output-dir ${QIIME_OUT_DIR}/denoise_output
+do_dada2 () {
+  qiime dada2 denoise-paired \
+    --i-demultiplexed-seqs ${QIIME_OUT_DIR}/seqs_paired_end.qza \
+    --p-trunc-len-f 250 \
+    --p-trunc-len-r 210 \
+    --p-max-ee-f 2 \
+    --p-max-ee-r 3 \
+    --p-n-threads 16 \
+    --output-dir ${QIIME_OUT_DIR}/denoise_output
+}
+
+do_deblur () {
+  qiime vsearch merge-pairs \
+    --i-demultiplexed-seqs ${QIIME_OUT_DIR}/seqs_paired_end.qza \
+    --o-merged-sequences ${QIIME_OUT_DIR}/seqs_joined.qza
+
+  qiime deblur denoise-16S \
+    --i-demultiplexed-seqs ${QIIME_OUT_DIR}/seqs_joined.qza \
+    --p-trim-length -1 \
+    --p-sample-stats \
+    --p-jobs-to-start 16 \
+    --p-min-reads 1 \
+    --output-dir ${QIIME_OUT_DIR}/denoise_output
+}
+
+if [ "$TOOL" = "DADA2" ]; then
+    do_dada2
+elif [ "$TOOL" = "DEBLUR" ]; then
+    do_deblur
+else
+    echo "Invalid tool specified. Please specify DADA2 or DEBLUR."
+fi
+
 
 qiime feature-table summarize \
  --i-table ${QIIME_OUT_DIR}/denoise_output/table.qza \
