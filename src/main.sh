659855b039943b2a8f577daa4d54b087957cfb52
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2024-07-19 01:20:17 -0700

    FEAT: more modular
    - relative pathing

--- src/main.sh
+++ src/main.sh
@@ -2,20 +2,22 @@
 #
 # Perform a series of standardized QIIME2 commands for importing data.
 
-FASTQ_DIR="/home/erick/projects/one-offs/20240416_edna/fastq"
-QIIME_OUT_DIR="/home/erick/projects/one-offs/20240416_edna/qiime_output"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(dirname ${SCRIPT_DIR})"
+FASTQ_DIR=${PROJECT_DIR}/fastq
+QIIME_OUT_DIR=${PROJECT_DIR}/qiime_output
 QIIME_DATA="/home/erick/data/qiime"
 
 #./src/_generate_manifest.sh ${FASTQ_DIR}
 
-./src/_import_data.sh \
+bash ./src/_import_data.sh \
   FASTQ_DIR=${FASTQ_DIR} \
   QIIME_OUT_DIR=${QIIME_OUT_DIR} \
   QIIME_DATA=${QIIME_DATA} \
   DENOISE="DADA2"
 
 read -p "Mean sample depth: " SAMPLE_DEPTH
-./src/_output_taxa_barplot.sh \
+bash ./src/_output_taxa_barplot.sh \
   FASTQ_DIR=${FASTQ_DIR} \
   QIIME_OUT_DIR=${QIIME_OUT_DIR} \
   QIIME_DATA=${QIIME_DATA} \
