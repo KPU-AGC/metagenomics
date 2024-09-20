7549ed81e4661d978df5a5251db9b203e5433dfb
PaddyKTaylor <paddyktaylor@gmail.com>
2024-09-20 16:03:05 -0700

    Uncommented commands

--- src/gen_manifest_to_barplot_qiime_analysis.sh
+++ src/gen_manifest_to_barplot_qiime_analysis.sh
@@ -21,8 +21,8 @@ EOF
 # *WHICH_RUN, IMPORT_FASTQS, and SAMPLE_SHEET need to be changed for each run
 # *Change WHICH_RUN for each dataset that you're analyzing
 # ---------------------------------------------------
-WHICH_RUN=20240911-Norgen_Nano_16S_ITS # Including this to help remove human error, just changing this one variable will enable formatting this workflow for any datasets
-FASTQS_LOC=fastq_trimmed/surviv_pairs_fastqs # This will vary between "Basecalls" and "Alignment_1"
+WHICH_RUN=20240911-Norgen_Nano_16S_ITS # Including this to help remove human error, just changing this and the next variable as pseudo-parameters will enable formatting this workflow for any datasets
+FASTQS_LOC=fastq_trimmed/surviv_pairs_fastqs # This will vary between "Basecalls", "Alignment_1", etc (as you can see here for example)
 # *Change the subfolder for IMPORT_FASTQS depending on the dataset
 # Root directories for the project
 # ---------------------------------------------------
@@ -60,16 +60,16 @@ FAITH_PD_VECTOR=${QIIME_ANALYSES}/core-metrics-results/faith_pd_vector.qza
 FAITH_PD_GROUP_SIGNIF_VIS=${QIIME_ANALYSES}/core-metrics-results/faith_pd_group_significance.qzv
 SHANNON_VECTOR=${QIIME_ANALYSES}/core-metrics-results/shannon_vector.qza
 SHANNON_GROUP_SIGNIF_VIS=${QIIME_ANALYSES}/core-metrics-results/shannon_group_significance.qzv
-## 16S Taxonomic Assignment
-## ---------------------------------------------------
-#16S_TAXONOMY_CLASSIFER=${RAW_DATA}/silva-138-99-nb-classifier.qza
-#16S_TAXONOMY_QZA=${QIIME_DATA}/16S_assigned_taxonomy.qza
-#16S_TAXONOMY_QZV=${QIIME_DATA}/16S_assigned_taxonomy.qzv
-## ITS Taxonomic Assignment
-## ---------------------------------------------------
-#ITS_TAXONOMY_CLASSIFER=${RAW_DATA}/...
-#ITS_TAXONOMY_QZA=${QIIME_DATA}/ITS_assigned_taxonomy.qza
-#ITS_TAXONOMY_QZV=${QIIME_DATA}/ITS_assigned_taxonomy.qzv
+# 16S Taxonomic Assignment
+# ---------------------------------------------------
+16S_TAXONOMY_CLASSIFER=${RAW_DATA}/silva-138-99-nb-classifier.qza
+16S_TAXONOMY_QZA=${QIIME_DATA}/16S_assigned_taxonomy.qza
+16S_TAXONOMY_QZV=${QIIME_DATA}/16S_assigned_taxonomy.qzv
+# ITS Taxonomic Assignment
+# ---------------------------------------------------
+ITS_TAXONOMY_CLASSIFER=${RAW_DATA}/...
+ITS_TAXONOMY_QZA=${QIIME_DATA}/ITS_assigned_taxonomy.qza
+ITS_TAXONOMY_QZV=${QIIME_DATA}/ITS_assigned_taxonomy.qzv
 # Visualization of alpha and beta diversity, and generate barplots 
 # ---------------------------------------------------
 ALPHA_RAREFACTION=${QIIME_ANALYSES}/alpha_rarefaction.qzv
@@ -97,9 +97,9 @@ echo -e "sample-id\tforward-absolute-filepath\treverse-absolute-filepath" > $ITS
 awk 'NR > 1 && /-ITS/' $IMPORT_MANIFEST >> $ITS_IMPORT_MANIFEST
 echo -e "sample-id\tforward-absolute-filepath\treverse-absolute-filepath" > $MC_IMPORT_MANIFEST
 awk 'NR > 1 && /MC1/' $IMPORT_MANIFEST >> $MC_IMPORT_MANIFEST
-## Sample metadata files
-## ---------------------------------------------------
-## I just made these manually in a spreadsheet, saved as a tsv, and copied them to Deoxys
+# Sample metadata files
+# ---------------------------------------------------
+# I just made these manually in a spreadsheet, saved as a tsv, and copied them to Deoxys
 # ---------------------------------------------------
 # CREATING QIIME2 ARTIFACTS
 # ---------------------------------------------------
@@ -128,99 +128,99 @@ qiime tools import \
   --input-format PairedEndFastqManifestPhred33V2 \
   --input-path $ITS_IMPORT_MANIFEST \
   --output-path $ITS_IMPORT_QZA
-#[ -r $IMPORT_QZA ] && echo "Dataset imported and Qiime artifact successfully generated"
-## ---------------------------------------------------
-## ASSIGN TRIMMING PARAMETERS AND GENERATE FEATURE TABLE
-## ---------------------------------------------------
-## Before carrying this out, be sure to review the fastqc results and determine a trim length for the dataset
-#echo "Sit back, this next step may take a little while (~10 min +/-10 min)"
-#qiime dada2 denoise-paired \
-#    --i-demultiplexed-seqs $IMPORT_QZA \
-#    --p-trim-left-f 0 \
-#    --p-trim-left-r 0 \
-#    --p-trunc-len-f 250 \
-#    --p-trunc-len-r 250 \
-#    --o-representative-sequences $REP_SEQS_QZA \
-#    --o-table $FEATURE_TABLE \
-#    --o-denoising-stats $REP_SEQS_STATS \
-#    --p-n-threads 16
-#qiime metadata tabulate \
-#    --m-input-file $REP_SEQS_STATS \
-#    --o-visualization $STATS_VIS
-#[ -r $STATS_VIS ] && echo "Dereplicated sequences, feature table, merged pairs, and clustered features artifacts have been generated. Check your Qiime visualizations for metrics of your results"
-## ---------------------------------------------------
-## GENERATE FEATURE TABLE SUMMARIES
-## ---------------------------------------------------
-#qiime feature-table summarize \
-#    --i-table $FEATURE_TABLE \
-#    --o-visualization $FEATURE_TABLE_VIS
-#qiime feature-table tabulate-seqs \
-#    --i-data $REP_SEQS_QZA \
-#    --o-visualization $REP_SEQS_QZV
-## ---------------------------------------------------
-## GENERATE PHYLOGENETIC TREE FOR DIVERSITY ANALYSIS
-## ---------------------------------------------------
-#qiime phylogeny align-to-tree-mafft-fasttree \
-#    --i-sequences $REP_SEQS_QZA \
-#    --o-alignment $MSA_QZA \
-#    --o-masked-alignment $MSA_MASKED_QZA \
-#    --o-tree $UNROOTED_TREE \
-#    --o-rooted-tree $ROOTED_TREE
-## ---------------------------------------------------
-## RAREFACTION ANALYSIS (ALPHA DIVERSITY)
-## ---------------------------------------------------
-#[ ! -d ${QIIME_ANALYSES} ] && mkdir -m 755 ${QIIME_ANALYSES}
-#qiime diversity core-metrics-phylogenetic \
-#    --i-phylogeny $ROOTED_TREE \
-#    --i-table $FEATURE_TABLE \
-#    --p-sampling-depth 350 \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --output-dir ${QIIME_ANALYSES}/core-metrics-results # HEADS-UP: Qiime2 will generate this folder itself. You can't path to an existing folder, Qiime will state the folder already exists and it will not overwrite. It'll then give errors that no --output-dir was provided
-#qiime diversity alpha-group-significance \
-#    --i-alpha-diversity $FAITH_PD_VECTOR \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --o-visualization $FAITH_PD_GROUP_SIGNIF_VIS
-#qiime diversity alpha-group-significance \
-#    --i-alpha-diversity $SHANNON_VECTOR \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --o-visualization $SHANNON_GROUP_SIGNIF_VIS
-## ---------------------------------------------------
-## RAREFACTION PLOTTING (i.e., GENERATING VISUALIZATIONS OF ALPHA DIVERSITY)
-## ---------------------------------------------------
-#qiime diversity alpha-rarefaction \
-#    --i-table $FEATURE_TABLE \
-#    --i-phylogeny $ROOTED_TREE \
-#    --p-max-depth 4000 \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --o-visualization $ALPHA_RAREFACTION
-## ---------------------------------------------------
-## VISUALIZING BETA DIVERSITY
-## ---------------------------------------------------
-#qiime diversity beta-group-significance \
-#    --i-distance-matrix $UW_UF_DIST_MAT \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --m-metadata-column 'Sample_Type' \
-#    --o-visualization $UW_UF_SAMPLE_TYPE_SIGNIF
-## ---------------------------------------------------
-## TAXONOMIC ASSIGNMENT
-## ---------------------------------------------------
-#qiime feature-classifier classify-sklearn \
-#    --i-classifier $TAXONOMY_CLASSIFER \
-#    --i-reads $REP_SEQS_QZA \
-#    --o-classification $TAXONOMY_QZA
-#qiime metadata tabulate \
-#    --m-input-file $TAXONOMY_QZA \
-#    --o-visualization $TAXONOMY_QZV
-## ---------------------------------------------------
-## VISUALIZING PROPORTIONS OF ASV AND TAXONOMIC MAKE UP FOR EACH SAMPLE 
-## ---------------------------------------------------
-#qiime taxa barplot \
-#    --i-table $FEATURE_TABLE \
-#    --i-taxonomy $TAXONOMY_QZA \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --o-visualization $TAXA_BARPLOT
-#qiime taxa barplot \
-#    --i-table $FEATURE_TABLE \
-#    --m-metadata-file $SAMPLE_METADATA \
-#    --o-visualization $ASV_BARPLOT
-#[ -r $ASV_BARPLOT ] && echo "Script completed. Check out all your artefacts and visualizations, see how they look!"
+[ -r $IMPORT_QZA ] && echo "Dataset imported and Qiime artifact successfully generated"
+# ---------------------------------------------------
+# ASSIGN TRIMMING PARAMETERS AND GENERATE FEATURE TABLE
+# ---------------------------------------------------
+# Before carrying this out, be sure to review the fastqc results and determine a trim length for the dataset
+echo "Sit back, this next step may take a little while (~10 min +/-10 min)"
+qiime dada2 denoise-paired \
+    --i-demultiplexed-seqs $IMPORT_QZA \
+    --p-trim-left-f 0 \
+    --p-trim-left-r 0 \
+    --p-trunc-len-f 250 \
+    --p-trunc-len-r 250 \
+    --o-representative-sequences $REP_SEQS_QZA \
+    --o-table $FEATURE_TABLE \
+    --o-denoising-stats $REP_SEQS_STATS \
+    --p-n-threads 16
+qiime metadata tabulate \
+    --m-input-file $REP_SEQS_STATS \
+    --o-visualization $STATS_VIS
+[ -r $STATS_VIS ] && echo "Dereplicated sequences, feature table, merged pairs, and clustered features artifacts have been generated. Check your Qiime visualizations for metrics of your results"
+# ---------------------------------------------------
+# GENERATE FEATURE TABLE SUMMARIES
+# ---------------------------------------------------
+qiime feature-table summarize \
+    --i-table $FEATURE_TABLE \
+    --o-visualization $FEATURE_TABLE_VIS
+qiime feature-table tabulate-seqs \
+    --i-data $REP_SEQS_QZA \
+    --o-visualization $REP_SEQS_QZV
+# ---------------------------------------------------
+# GENERATE PHYLOGENETIC TREE FOR DIVERSITY ANALYSIS
+# ---------------------------------------------------
+qiime phylogeny align-to-tree-mafft-fasttree \
+    --i-sequences $REP_SEQS_QZA \
+    --o-alignment $MSA_QZA \
+    --o-masked-alignment $MSA_MASKED_QZA \
+    --o-tree $UNROOTED_TREE \
+    --o-rooted-tree $ROOTED_TREE
+# ---------------------------------------------------
+# RAREFACTION ANALYSIS (ALPHA DIVERSITY)
+# ---------------------------------------------------
+[ ! -d ${QIIME_ANALYSES} ] && mkdir -m 755 ${QIIME_ANALYSES}
+qiime diversity core-metrics-phylogenetic \
+    --i-phylogeny $ROOTED_TREE \
+    --i-table $FEATURE_TABLE \
+    --p-sampling-depth 350 \
+    --m-metadata-file $SAMPLE_METADATA \
+    --output-dir ${QIIME_ANALYSES}/core-metrics-results # HEADS-UP: Qiime2 will generate this folder itself. You can't path to an existing folder, Qiime will state the folder already exists and it will not overwrite. It'll then give errors that no --output-dir was provided
+qiime diversity alpha-group-significance \
+    --i-alpha-diversity $FAITH_PD_VECTOR \
+    --m-metadata-file $SAMPLE_METADATA \
+    --o-visualization $FAITH_PD_GROUP_SIGNIF_VIS
+qiime diversity alpha-group-significance \
+    --i-alpha-diversity $SHANNON_VECTOR \
+    --m-metadata-file $SAMPLE_METADATA \
+    --o-visualization $SHANNON_GROUP_SIGNIF_VIS
+# ---------------------------------------------------
+# RAREFACTION PLOTTING (i.e., GENERATING VISUALIZATIONS OF ALPHA DIVERSITY)
+# ---------------------------------------------------
+qiime diversity alpha-rarefaction \
+    --i-table $FEATURE_TABLE \
+    --i-phylogeny $ROOTED_TREE \
+    --p-max-depth 4000 \
+    --m-metadata-file $SAMPLE_METADATA \
+    --o-visualization $ALPHA_RAREFACTION
+# ---------------------------------------------------
+# VISUALIZING BETA DIVERSITY
+# ---------------------------------------------------
+qiime diversity beta-group-significance \
+    --i-distance-matrix $UW_UF_DIST_MAT \
+    --m-metadata-file $SAMPLE_METADATA \
+    --m-metadata-column 'Sample_Type' \
+    --o-visualization $UW_UF_SAMPLE_TYPE_SIGNIF
+# ---------------------------------------------------
+# TAXONOMIC ASSIGNMENT
+# ---------------------------------------------------
+qiime feature-classifier classify-sklearn \
+    --i-classifier $TAXONOMY_CLASSIFER \
+    --i-reads $REP_SEQS_QZA \
+    --o-classification $TAXONOMY_QZA
+qiime metadata tabulate \
+    --m-input-file $TAXONOMY_QZA \
+    --o-visualization $TAXONOMY_QZV
+# ---------------------------------------------------
+# VISUALIZING PROPORTIONS OF ASV AND TAXONOMIC MAKE UP FOR EACH SAMPLE 
+# ---------------------------------------------------
+qiime taxa barplot \
+    --i-table $FEATURE_TABLE \
+    --i-taxonomy $TAXONOMY_QZA \
+    --m-metadata-file $SAMPLE_METADATA \
+    --o-visualization $TAXA_BARPLOT
+qiime taxa barplot \
+    --i-table $FEATURE_TABLE \
+    --m-metadata-file $SAMPLE_METADATA \
+    --o-visualization $ASV_BARPLOT
+[ -r $ASV_BARPLOT ] && echo "Script completed. Check out all your artefacts and visualizations, see how they look!"
--- src/trim_and_QC_fastqs.sh
+++ src/trim_and_QC_fastqs.sh
@@ -19,8 +19,7 @@ EOF
 # *Change WHICH_RUN for each dataset that you're analyzing
 # ---------------------------------------------------
 WHICH_RUN=20240911-Norgen_Nano_16S_ITS # Including this to help remove human error, just changing this one variable will enable formatting this workflow for any datasets
-FASTQS_LOC=Fastq # This will vary between "Basecalls" and "Alignment_1"
-# *Change the subfolder for IMPORT_FASTQS depending on the dataset
+FASTQS_LOC=Fastq # This will vary between "Basecalls", "Fastq", or "Alignment_1"
 # Root directories for the project
 # ---------------------------------------------------
 RAW_DATA=~/Documents/paddy/NGS_microbial_samples_testing/data/raw_data
@@ -38,53 +37,53 @@ ADAPTER_FASTA=${RAW_DATA}/$WHICH_RUN/adapters_for_trimming.fa
 # i.e., any different sequences then indicated targeting ITS or 16S region
 TEMP_UNZIP_FOLDER=${ANALYZED_DATA}/$WHICH_RUN/temp_unzipped_fastqs
 TRIM_FQ_FRAGMENT_COUNTS=${ANALYZED_DATA}/$WHICH_RUN/trimmed_fastq_fragment_counts.csv
-## ---------------------------------------------------
-## PRE-TRIM QC (.fastq FOR .html REPORT)
-## ---------------------------------------------------
-#[ ! -d ${PRETRIM_FASTQC} ] && mkdir -m 755 ${PRETRIM_FASTQC}
-#fastqc -t 16 -o ${PRETRIM_FASTQC} ${FASTQS}/*.fastq.gz
-#multiqc ${PRETRIM_FASTQC} --title $WHICH_RUN --filename $WHICH_RUN --outdir ${PRETRIM_FASTQC}
-#[ -r ${PRETRIM_FASTQC}/"$WHICH_RUN".html ] && echo "QC has completed"
+# ---------------------------------------------------
+# PRE-TRIM QC (.fastq FOR .html REPORT)
+# ---------------------------------------------------
+[ ! -d ${PRETRIM_FASTQC} ] && mkdir -m 755 ${PRETRIM_FASTQC}
+fastqc -t 16 -o ${PRETRIM_FASTQC} ${FASTQS}/*.fastq.gz
+multiqc ${PRETRIM_FASTQC} --title $WHICH_RUN --filename $WHICH_RUN --outdir ${PRETRIM_FASTQC}
+[ -r ${PRETRIM_FASTQC}/"$WHICH_RUN".html ] && echo "QC has completed"
 # ---------------------------------------------------
 # PERFORM TRIMMING (.fastq ONLY, OUTPUT IS .fastq)
 # ---------------------------------------------------
-#[ ! -d ${TRIM_FASTQS} ] && mkdir -m 755 ${TRIM_FASTQS}
-#[ ! -d ${TRIM_SURV_FASTQS} ] && mkdir -m 755 ${TRIM_SURV_FASTQS}
-#[ ! -d ${TRIM_ORPH_FASTQS} ] && mkdir -m 755 ${TRIM_ORPH_FASTQS}
-#ls ${FASTQS} | grep .fastq.gz | sed 's/_L001.*/_L001/' | sort | uniq > $SAMPLE_LIST
-#while IFS= read -r SAMPLE_ID; do
-#    trimmomatic \
-#        PE \
-#        -threads 16 \
-#        -phred33 \
-#        -trimlog ${TRIM_FASTQS}/20240717_trim.log \
-#        ${FASTQS}/"$SAMPLE_ID"_R1_001.fastq.gz \
-#        ${FASTQS}/"$SAMPLE_ID"_R2_001.fastq.gz \
-#        ${TRIM_SURV_FASTQS}/trimmed-surviv-pairs-"$SAMPLE_ID"_R1_001.fastq.gz \
-#        ${TRIM_ORPH_FASTQS}/trimmed-orphan-pairs-"$SAMPLE_ID"_R1_001.fastq.gz \
-#        ${TRIM_SURV_FASTQS}/trimmed-surviv-pairs-"$SAMPLE_ID"_R2_001.fastq.gz \
-#        ${TRIM_ORPH_FASTQS}/trimmed-orphan-pairs-"$SAMPLE_ID"_R2_001.fastq.gz \
-#        ILLUMINACLIP:$ADAPTER_FASTA:2:30:10:2:True SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:36
-#done < $SAMPLE_LIST
-#if ls "${TRIM_SURV_FASTQS}"/*.fastq.gz 1>/dev/null 2>&1; then
-#    echo "Adapter trimming has completed. Be sure to check that all your sample fastqs are accounted for"
-#    exit 0
-#else
-#    echo "There was an issue with trimming fastqs. Check your files and your script before trying again"
-#    exit 1
-#fi
-## ---------------------------------------------------
-## UNZIP ALL THE TRIMMED FASTQS FOR THE DOWNSTREAM CHECK FOR PARITY
-## ---------------------------------------------------
+[ ! -d ${TRIM_FASTQS} ] && mkdir -m 755 ${TRIM_FASTQS}
+[ ! -d ${TRIM_SURV_FASTQS} ] && mkdir -m 755 ${TRIM_SURV_FASTQS}
+[ ! -d ${TRIM_ORPH_FASTQS} ] && mkdir -m 755 ${TRIM_ORPH_FASTQS}
+ls ${FASTQS} | grep .fastq.gz | sed 's/_L001.*/_L001/' | sort | uniq > $SAMPLE_LIST
+while IFS= read -r SAMPLE_ID; do
+    trimmomatic \
+        PE \
+        -threads 16 \
+        -phred33 \
+        -trimlog ${TRIM_FASTQS}/20240717_trim.log \
+        ${FASTQS}/"$SAMPLE_ID"_R1_001.fastq.gz \
+        ${FASTQS}/"$SAMPLE_ID"_R2_001.fastq.gz \
+        ${TRIM_SURV_FASTQS}/trimmed-surviv-pairs-"$SAMPLE_ID"_R1_001.fastq.gz \
+        ${TRIM_ORPH_FASTQS}/trimmed-orphan-pairs-"$SAMPLE_ID"_R1_001.fastq.gz \
+        ${TRIM_SURV_FASTQS}/trimmed-surviv-pairs-"$SAMPLE_ID"_R2_001.fastq.gz \
+        ${TRIM_ORPH_FASTQS}/trimmed-orphan-pairs-"$SAMPLE_ID"_R2_001.fastq.gz \
+        ILLUMINACLIP:$ADAPTER_FASTA:2:30:10:2:True SLIDINGWINDOW:4:20 LEADING:3 TRAILING:3 MINLEN:36
+done < $SAMPLE_LIST
+if ls "${TRIM_SURV_FASTQS}"/*.fastq.gz 1>/dev/null 2>&1; then
+    echo "Adapter trimming has completed. Be sure to check that all your sample fastqs are accounted for"
+    exit 0
+else
+    echo "There was an issue with trimming fastqs. Check your files and your script before trying again"
+    exit 1
+fi
+# ---------------------------------------------------
+# UNZIP ALL THE TRIMMED FASTQS FOR THE DOWNSTREAM CHECK FOR PARITY
+# ---------------------------------------------------
 [ ! -d ${TEMP_UNZIP_FOLDER} ] && mkdir -m 755 ${TEMP_UNZIP_FOLDER}
 for FILE in "${TRIM_SURV_FASTQS}"/*.gz; do
     BASENAME=$(basename "$FILE" .gz)
     gunzip -c "$FILE" > "${TEMP_UNZIP_FOLDER}/${BASENAME}"
 done
-## ---------------------------------------------------
-## NOW CHECK FOR PARITY IN THE NUMBER OF FRAGMENT COUNTS FOR TRIMMED PAIRED-END FASTQS
-## ---------------------------------------------------
-#echo "Sample_fastq,R1_count,R2_count" > $TRIM_FQ_FRAGMENT_COUNTS
+# ---------------------------------------------------
+# NOW CHECK FOR PARITY IN THE NUMBER OF FRAGMENT COUNTS FOR TRIMMED PAIRED-END FASTQS
+# ---------------------------------------------------
+echo "Sample_fastq,R1_count,R2_count" > $TRIM_FQ_FRAGMENT_COUNTS
 for FILE in "${TEMP_UNZIP_FOLDER}"/*R1*; do
     SAMPLE_NAME=$(basename "${FILE/_L001_R1_001.fastq}")
     R1_COUNTS=$(wc -l < "$FILE")
